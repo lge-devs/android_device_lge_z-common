c6bc6071283cc34b8bc6a4f0179f6b0191d87e92
Ethan Chen <intervigil@gmail.com>
2017-09-16 23:19:53 -0700

    z-common: Add vibrator HAL in place of CMHW implementation
    Change-Id: I19d4f916d807301ec30b882bfdeaf76715a041c7
    (cherry picked from commit ed0299b45c2c6f36d3003917405095839c753b57)

--- zee.mk
+++ zee.mk
@@ -258,7 +258,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.1-service.z
+    android.hardware.vibrator@1.0-service.z
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
